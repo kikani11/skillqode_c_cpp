1b2739bf855800f434491555f33b9d64bf15efa7
vaidik <kikanivaidik@gmail.com>
2022-10-11 11:10:29 +0530

    operoverloading

--- Bankstatice.cpp
+++ Bankstatice.cpp
@@ -1,69 +1,85 @@
 #include<iostream>
-#include<stdio.h>
-#include<string.h>
-
 using namespace std;
-
 class bank
 {
-        int acno;
-        char nm[100], acctype[100];
-        float bal;  
-   public:
-        bank(int acc_no, char *name, char *acc_type, float balance)  //Parameterized Constructor
-        {
-                acno=acc_no;
-                strcpy(nm, name);
-                strcpy(acctype, acc_type);
-                bal=balance;
-        }
-        void deposit();
-        void withdraw();
-        void display();
+    protected:
+    int accno;
+    public:
+    void acc_no(int num)
+    {
+        accno=num;
+    }
+    int getacc_no()
+    {
+        return accno;
+    }
 };
-void bank::deposit()   //depositing an amount
+class saving_account:public bank
 {
-        int damt1;
-        cout<<"\n Enter Deposit Amount = ";
-        cin>>damt1;
-        bal+=damt1;
-}
-void bank::withdraw()  //withdrawing an amount
-{
-        int wamt1;
-        cout<<"\n Enter Withdraw Amount = ";
-        cin>>wamt1;
-        if(wamt1>bal)
-                cout<<"\n Cannot Withdraw Amount";
-        bal-=wamt1;
-}
-void bank::display()  //displaying the details
-{
-        cout<<"\n ----------------------";
-        cout<<"\n Accout No. : "<<acno;
-        cout<<"\n Name : "<<nm;
-        cout<<"\n Account Type : "<<acctype;
-        cout<<"\n Balance : "<<bal;  
-}
+    int bal,dep,with;
+    public:
+    saving_account(int amt)
+    {
+    bal=amt;
+    }
+    void acc_no2()
+    {
+        cout<<"\n your account no:"<<accno;
+    }
+    void deposit(int dep)
+    {
+    bal+=dep;
+    }
+    void withdraw(int with)
+    {
+    bal-=with;
+    }
+    int showbal()   
+    {
+    return bal;
+    }
+};
+
 int main()
 {
-        int acc_no;
-        char name[100], acc_type[100];
-        float balance;
-        cout<<"\n Enter Details: \n";
-        cout<<"-----------------------";
-        cout<<"\n Accout No. ";
-        cin>>acc_no;
-        cout<<"\n Name : ";
-        cin>>name;
-        cout<<"\n Account Type : ";
-        cin>>acc_type;
-        cout<<"\n Balance : ";
-        cin>>balance;
+    int dep,with,n;
+    int accno,num;
+    saving_account rahul(9999);
+    rahul.acc_no(122);
+    rahul.acc_no2();
+
+    cout<<"\n if you enter dep amt: 'press 1'";
+    cout<<"\n if you enter withdraw amt: 'press 2'";
+    cout<<"\n if you show balance: 'press 9'"; 
+    cout<<"\n if you exit: 'press 0'"; 
+
+    do
+    {
+    cout<<"\n enter n:";
+    cin>>n;
+
+    switch(n)
+    {
+        case 0:
+            break;
+        case 1:
+            cout<<"\n enter deposit:";
+            cin>>dep;
+            rahul.deposit(dep);
+            break;
+        case 2:
+
+            cout<<"\n enter withdraw:";
+            cin>>with;
+            rahul.withdraw(with);
+            break;
+        case 9:
+            cout<<"\n balance:"<<rahul.showbal();
+            break;
+        default:
+            cout<<"\n wrong choise";
+            break;
+    }
+    }while (n!=0);
   
-        bank b1(acc_no, name, acc_type, balance);  //object is created
-        b1.deposit(); //
-        b1.withdraw(); // calling member functions
-        b1.display(); //
-        return 0;
 }
